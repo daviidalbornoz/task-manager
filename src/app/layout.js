cf3a1cd123399cbe8bd7b7c9702a4c2d26f8785a
David Albornoz <albornozd@usf.edu>
2026-04-12 22:24:50 -0400

    implement neumorphic design system and custom layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-screen flex flex-col bg-[#e0e5ec]">
+  {children}
+      </body>
     </html>
   );
 }
